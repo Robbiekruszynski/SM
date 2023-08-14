05b1d05f925c1c09b8d8252a6fc27134afe93b03
robbie <robbiekruszynski@gmail.com>
2023-08-14 15:40:37 +0200

    update index

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-// import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { Canvas } from '@react-three/fiber'
+import './styles.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Canvas camera={{ position: [0, 0, 2] }}>
+      <mesh>
+        <boxGeometry />
+        <meshBasicMaterial color={0x00ff00} wireframe />
+      </mesh>
+    </Canvas>
+  </StrictMode>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
