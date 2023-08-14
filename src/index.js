0bbe7f48160d37633c1fe1127cda4f678c7ee9aa
robbie <robbiekruszynski@gmail.com>
2023-08-14 15:57:36 +0200

    add styles

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
-// import './styles.css'
+import './styles.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -14,8 +14,3 @@ createRoot(document.getElementById('root')).render(
     </Canvas>
   </StrictMode>
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
