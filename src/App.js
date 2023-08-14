f4f92133006fa9c4ff67fe9dd5bf79fad6dab707
robbie <robbiekruszynski@gmail.com>
2023-08-14 16:46:06 +0200

    add props + another box for demo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Threejs from "./Three"
 import { Canvas } from '@react-three/fiber'
-import Box from './components/Box'
+import Box from './components/threejs/Box'
 
 export default function App() {
   return (
     <Canvas camera={{ position: [0, 0, 2] }}>
-      <Box />
+      <Box position={[-0.75, 0, 0]} name="1" />
+      <Box position={[0.75, 0, 0]} name="2" />
+    
     </Canvas>
   )
 }
