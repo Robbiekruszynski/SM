07989da05e362087219b02e27234d6303e3745ae
robbie <robbiekruszynski@gmail.com>
2023-08-14 19:00:56 +0200

    fix position

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import Box from './components/threejs/Box'
 
 export default function App() {
   return (
-    <Canvas camera={{ position: [0, 0, 2] }}>
-      <Box position={[-0.75, 0, 0]} name="A"/>
-      <Box position={[0.75, 0, 0]} name="B"/>
+    <Canvas camera={{ position: [0, 5, 2] }}>
+      <Box position={[-0.1, 0, 0]} name="A"/>
+      {/* <Box position={[0.75, 0, 0]} name="B"/> */}
     </Canvas>
   )
 }
