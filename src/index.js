75755cd2f43a8f80817ae27bf3cc765e276c2fdd
robbie <robbiekruszynski@gmail.com>
2023-08-14 15:45:52 +0200

    inject fiber in index

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
-import './styles.css'
+// import './styles.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
