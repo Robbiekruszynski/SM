9d46d8872d1d05d86da573d073e5f92e50301b67
robbie <robbiekruszynski@gmail.com>
2023-08-14 18:45:46 +0200

    roatation lags, why why why

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Canvas } from '@react-three/fiber'
 import Box from './components/threejs/Box'
 
@@ -8,7 +8,7 @@ export default function App() {
   return (
     <Canvas camera={{ position: [0, 0, 2] }} frameloop="demand">
       <Box position={[-0.75, 0, 0]} name="1" />
-      <Box position={[0.75, 0, 0]} name="2" />
+      {/* <Box position={[0.75, 0, 0]} name="2" /> */}
     
     </Canvas>
   )
--- src/components/threejs/Box.js
+++ src/components/threejs/Box.js
@@ -1,30 +1,21 @@
-import {useRef, useEffect} from 'react'
+import { useRef } from 'react'
 import { useFrame } from '@react-three/fiber'
-
 
 export default function Box(props) {
   const ref = useRef()
 
-  useEffect(() => {
-    if (ref.current.name === '2') {
-      ref.current.position.y = .5
-
-    }
-  })
-
   useFrame((_, delta) => {
-    ref.current.rotation.x += 1 * delta
-    ref.current.rotation.y += 0.5 * delta
+    ref.current.rotation.x += 6 * delta
+    ref.current.rotation.y += 5 * delta
   })
 
-    return (
-      <mesh
-        {...props} ref={ref}>
-        <boxGeometry />
-        <meshBasicMaterial color={0x00ff00} wireframe />
-      </mesh>
-    )
-  }
+  return (
+    <mesh {...props} ref={ref}>
+      <boxGeometry />
+      <meshBasicMaterial color={0x00ff00} wireframe />
+    </mesh>
+  )
+}
 
 
   //you could move an object at a speed of 10 units per second.
