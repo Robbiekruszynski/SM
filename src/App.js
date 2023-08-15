1839acd1d10aee1c28d3c8f856644d67d2cee936
robbie <robbiekruszynski@gmail.com>
2023-08-15 15:10:46 +0200

    add new vector and shader

--- src/App.js
+++ src/App.js
@@ -1,16 +1,83 @@
 
-import './App.css';
-import { Canvas } from '@react-three/fiber'
-import Box from './components/threejs/Box'
-// import Sphere from './components/threejs/Sphere'
+import { OrbitControls } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useMemo, useRef } from "react";
+import { MathUtils } from "three";
+import vertexShader from './components/threejs/VertexShader';
+import fragmentShader from './components/threejs/FragmentShader';
 
+const Blob = () => {
+  // This reference will give us direct access to the mesh
+  const mesh = useRef();
+  const hover = useRef(false);
 
+  const uniforms = useMemo(
+    () => ({
+      u_intensity: {
+        value: 0.3,
+      },
+      u_time: {
+        value: 0.0,
+      },
+    }),
+    []
+  );
 
-export default function App() {
+  useFrame((state) => {
+    const { clock } = state;
+    mesh.current.material.uniforms.u_time.value = 0.4 * clock.getElapsedTime();
+
+    mesh.current.material.uniforms.u_intensity.value = MathUtils.lerp(
+      mesh.current.material.uniforms.u_intensity.value,
+      hover.current ? 0.85 : 0.15,
+      0.02
+    );
+  });
+
   return (
-    <Canvas camera={{ position: [0, 5, 2] }}>
-      <Box position={[-0.0, 0, 0]} name="1"/>
-    </Canvas>
-  )
-}
+    <mesh
+      ref={mesh}
+      position={[0, 0, 0]}
+      scale={1.5}
+      onPointerOver={() => (hover.current = true)}
+      onPointerOut={() => (hover.current = false)}
+    >
+      <icosahedronGeometry args={[2, 20]} />
+      <shaderMaterial
+        fragmentShader={fragmentShader}
+        vertexShader={vertexShader}
+        uniforms={uniforms}
+        wireframe={true}
+      />
+    </mesh>
+  );
+};
 
+const Scene = () => {
+  return (
+    <Canvas camera={{ position: [0.0, 0.0, 8.0] }}>
+      <Blob />
+      <axesHelper />
+      <OrbitControls />
+    </Canvas>
+  );
+};
+
+export default Scene;
+
+
+//Box ref 
+// import './App.css';
+// import { Canvas } from '@react-three/fiber'
+// import Box from './components/threejs/Box'
+// // import Sphere from './components/threejs/Sphere'
+
+
+
+// export default function App() {
+//   return (
+//     <Canvas camera={{ position: [0, 5, 2] }}>
+//       <Box position={[-0.0, 0, 0]} name="1"/>
+//     </Canvas>
+//   )
+// }
---
+++ src/components/threejs/Sphere.js
@@ -0,0 +1,7 @@
+import { OrbitControls, Sphere } from "@react-three/drei";
+import { Canvas } from "react-three/fiber"
+import "./App.css";
+
+function ReflectiveSphere() : Element {
+
+}
